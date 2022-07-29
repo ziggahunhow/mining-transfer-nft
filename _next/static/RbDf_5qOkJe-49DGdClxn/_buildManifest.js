553b135210d531f0c723195f3f08305c60649e2c
ziggahunhow <petelinsw@gmail.com>
2022-07-29 10:42:18 +0000

    Deploying to gh-pages from @ ziggahunhow/mining-transfer-nft@616f9c458f01e98d2cb1ff272486b009d0f0129c 🚀

--- _next/static/RbDf_5qOkJe-49DGdClxn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6a7d3d9b041c7b7a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
